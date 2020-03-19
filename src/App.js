dfc2a39619fbfae3c17c7958c43bdef1a431a5fe
PiskoOo <piskooooooo@gmail.com>
2020-03-19 18:21:51 +0000

    fixed the sign out button not changing issue

--- src/App.js
+++ src/App.js
@@ -32,8 +32,8 @@ class App extends React.Component {
             }
           })
         })
-        this.setState({ currentUser: userAuth })
       }
+      this.setState({ currentUser: userAuth })
     });
   }
   
