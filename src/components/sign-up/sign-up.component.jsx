90c981b84f1f17e2edde0450f1244070f01aeea3
PiskoOo <piskooooooo@gmail.com>
2020-06-29 18:38:00 +0100

    moved functionality with sagas

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+import { signUpStart } from '../../redux/user/user.actions';
 
 import { SignUpContainer } from './sign-up.styles';
 
@@ -20,35 +21,22 @@ class SignUp extends React.Component {
     }
 
     handleSubmit = async event => {
-        event.preventDefault()
-        
+        event.preventDefault();
+        const { signUpStart } = this.props;
         const { displayName, email, password, confirmPassword } = this.state
         if(password !== confirmPassword) {
             alert("passwords don't match")
-            return
+            return;
         }
 
-        try {
-            const { user } = await auth.createUserWithEmailAndPassword(email, password)
-
-            await createUserProfileDocument(user, { displayName })
-
-            this.state({
-                displayName: '',
-                email: '',
-                password: '',
-                confirmPassword: ''
-            })
-        } catch (error) {
-            console.error(error)
-        }
-    }
+        signUpStart({ displayName, email, password });
+    };
 
     handleChange = event => {
         const { name, value } = event.target
 
         this.setState({ [name]: value })
-    }
+    };
 
     render() {
         const { displayName, email, password, confirmPassword } = this.state
@@ -96,4 +84,8 @@ class SignUp extends React.Component {
     }
 }
 
-export default SignUp;
+const mapDispatchToProps = dispatch => ({
+    signUpStart: userCredentials => dispatch(signUpStart(userCredentials))
+})
+
+export default connect(null, mapDispatchToProps)(SignUp);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -4,7 +4,7 @@ export const googleSignInStart = () => ({
     type: UserActionTypes.GOOGLE_SIGN_IN_START
 });
 
-export const signInSuccess = (user) => ({
+export const signInSuccess = user => ({
     type: UserActionTypes.SIGN_IN_SUCCESS,
     payload:user
 });
@@ -31,7 +31,22 @@ export const signOutSuccess = () => ({
     type: UserActionTypes.SIGN_OUT_SUCCESS
 });
 
-export const signOutFailure = (error) => ({
+export const signOutFailure = error => ({
     type: UserActionTypes.SIGN_OUT_FAILURE,
+    payload: error
+});
+
+export const signUpStart = userCredentials => ({
+    type: UserActionTypes.SIGN_UP_START,
+    payload: userCredentials
+});
+
+export const signUpSuccess = ({ user, additionalData }) => ({
+    type: UserActionTypes.SIGN_UP_SUCCESS,
+    payload: { user, additionalData }
+});
+
+export const signUpFailure = error => ({
+    type: UserActionTypes.SIGN_UP_FAILURE,
     payload: error
 });
